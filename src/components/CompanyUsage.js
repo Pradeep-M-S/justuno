fedb0aac3513d3b969216a9969411262d65a48f2
Pradeep M S <pradeep.saravanan@mallow-tech.com>
2022-05-18 22:32:46 +0530

    company usage page added

---
+++ src/components/CompanyUsage.js
@@ -0,0 +1,15 @@
+import "../App.css";
+
+const CompanyUsage = () => {
+    return (
+        <div className="company-usage-container">
+            <img src='https://www.justuno.com/wp-content/uploads/2021/07/logo-vitaminshoppe.svg' alt='company-img' />
+            <img src='https://www.justuno.com/wp-content/uploads/2021/07/logo-golf-channel-nbc.svg' alt='company-img' />
+            <img src='https://www.justuno.com/wp-content/uploads/2021/07/logo-volcom.svg' alt='company-img' />
+            <img src='https://www.justuno.com/wp-content/uploads/2021/07/logo-trek.svg' alt='company-img' />
+            <img src='https://www.justuno.com/wp-content/uploads/2021/07/logo-peter-millar-3.svg' alt='company-img' />
+        </div>
+    )
+}
+
+export default CompanyUsage;
